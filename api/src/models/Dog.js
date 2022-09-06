ad16319b36c44b180a8ddf0c7c5cefdc8e4a3110
red1730 <ducret.raul@gmail.com>
2022-09-06 17:43:30 -0300

    set DB

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,32 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
+    ID : {
+      type: DataTypes.UUID, // hace un numero random alfanum para evitar coliciones
+      defaultValue: DataTypes.UUID,
       allowNull: false,
+      primaryKey: true
     },
-  });
-};
+    name : {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    altura : {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    peso : {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    anosDeVida : {
+        type: DataTypes.INTEGER,
+        allowNull: true
+     },
+    createInDb : {
+        type : DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+    }
+  })
+}
