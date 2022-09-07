d7d5af0fcb9ea94f3619687b3a3a72203d5ac1f2
red1730 <ducret.raul@gmail.com>
2022-09-06 23:04:26 -0300

    routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,60 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const axios = require ('axios');
 
+const { Dog, Temp} = require ('../db.js')
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+const getApiInfo = async () => {
+    const apiUrl = await axios.get ('https://api.thedogapi.com/v1/breeds')
+    const apiInfo = await apiUrl.data.map ( el => {
+        return {
+            id:  el.id,
+            name: el.name,
+            altura: el.height.metric,
+            peso: el.weight.metric,
+            anosDeVida: el.life_span,
+            imagen: el.image.url
+        };
+    });
+    return apiInfo
+};
+
+const getDbInfo = async () => {
+    return await Dog.findAll({ // hago un fin.all para que me traiga todo de la tabla Dog
+        include: {
+            model: Temp, // y a su vez incluya la tabla Temp
+            attributes :['name'], // 
+            through: { // mediante los atributos, comprobacion
+                attributes: [],
+            }
+        },
+    })
+};
+
+const getAllDogs = async () => {
+ const infoApi = await getApiInfo();
+ const infoDb = await getDbInfo() ;
+ const infoTt = infoApi.concat(infoDb);
+ return infoTt
+}
+
+router.get ("/dogs", async (req,res) =>{
+    const name = req.query.name
+    let dogsTotal = await getAllDogs()
+    if(name){
+    const dogName = await dogsTotal.filter(n => n.name.toLowerCase().includes(name.toLowerCase()))
+    dogName.length ?
+    res.status(200).send(dogName) :
+    res.status(404).send(`Lo sentimos, ${name}, no se encontro.`)
+    } else {
+        res.status(200).send(dogsTotal)
+    }
+})
 
 module.exports = router;
