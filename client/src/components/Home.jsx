65094beca161b8fe98397a46163618dc98697fa6
red1730 <ducret.raul@gmail.com>
2022-09-10 15:50:21 -0300

    home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,3 +2,24 @@ import React from "react";
 import {useState, useEffect} from "react";
 import { useDispatch, useSelector} from "react-redux"
 import { getDog } from "../actions";
+import { Link } from "react-router-dom"; 
+
+
+export default function Home (){
+  
+  
+  const dipatch = useDispatch()
+  const allDogs = useSelector ((state) => state.dogs) 
+
+  useEffect (() => {
+    dispatchEvent(getDog());
+  },[])
+
+  return (
+<div>
+  <Link to= "dogs">Create race</Link>
+<h1>Udog</h1>
+</div>
+
+  )
+}
